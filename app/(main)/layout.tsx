04ede2ae17e7fb82a006a22d28cfd64396886ff6
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-28 16:37:54 +0700

    refactor: change layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils"
+import Footer from "@/components/layout/footer"
+import Header from "@/components/layout/header"
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  )
+}
